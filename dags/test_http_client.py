368f84d4b36299ccf02e017c6b700f97c49b2555
Gabriellyan Catheline <gabriellyan@pintu.co.id>
2025-04-08 13:43:35 +0700

    latest

--- dags/test_http_client.py
+++ dags/test_http_client.py
@@ -5,6 +5,8 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.operators.bash import BashOperator
 import logging
+from airflow.exceptions import AirflowSkipException
+import time
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -14,7 +16,12 @@ logging.basicConfig(
     datefmt='%Y-%m-%d %H:%M:%S'
 )
 
+def cb(context):
+    logger.debug('cbbbb')
+
+
 def metrics(**kwargs):
+    # raise AirflowSkipException("Skipping this task")    
     logger.info('info log1')
     logger.debug('debug logs1')
     logger.warning('warn log1')
@@ -28,28 +35,36 @@ def metrics(**kwargs):
     }, params={
         "detail": "full"
     })
-
+    time.sleep(10)
     print(res.status_code, res.json())
 
-
-dag = factory.create_monitored_dag(dag=DAG(
-    "testing_http_client",
+dag = factory.create_monitored_dag(
+    dag_id="testing_http_client",
     default_args={
         "depends_on_past": False,
-        "email": ["airflow@example.com"],
         "email_on_failure": False,
         "email_on_retry": False,
         "retries": 1,
+        "sla": timedelta(milliseconds=1),
     },
     description="Another simple tutorial DAG",
-    schedule=timedelta(seconds=600),
     start_date=datetime(2021, 1, 1),
     catchup=False,
-), sla=timedelta(seconds=30), team='cfp')
+    team='cfp',
+    owner="asd@asd.com",
+    email=["asd@asd.com"],
+    dagrun_timeout=timedelta(seconds=300),
+    tags=["cfp"],
+    max_active_runs=1,
+    schedule_interval='*/1 * * * *',
+    access_control={},
+)
+callbacks = dag.default_args['on_success_callback']
+callbacks.append(cb)
+
 with dag:
 
     t1 = PythonOperator(task_id="my_metrics", python_callable=metrics)
-
     t2 = BashOperator(
         task_id="print_date",
         bash_command="date",
--- testing.py
+++ testing.py
@@ -1,22 +1,10 @@
-from flask import Flask, Response
-import requests
+from flask import Flask, redirect
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
-def proxy_password():
-    target_url = 'https://ptu-ctf.s3.ap-southeast-1.amazonaws.com/password.txt'
-    try:
-        # Fetch the resource from the target URL
-        response = requests.get(target_url)
-        response.raise_for_status()  # Raise an error for HTTP error responses
-
-        # Return the fetched content to the client
-        return Response(response.text, content_type='text/plain')
-    except requests.exceptions.RequestException as e:
-        # Handle errors
-        return Response(f"Error fetching the resource: {e}", status=500)
+@app.route('/redirect')
+def redir():
+    return redirect("http://w49m1:s3cr3t@localhost:1337/admin", code=302)
 
 if __name__ == '__main__':
-    # Run the Flask app on localhost:3000
     app.run(host='0.0.0.0', port=8000)
